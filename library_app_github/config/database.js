7cbdcd4e217493fca2ca87bdda7ad6def6f32ddc
S Nandhakishor <nandakishornair17@gmail.com>
2022-08-02 13:54:45 +0530

    ist

---
+++ library_app_github/config/database.js
@@ -0,0 +1,5 @@
+// module.exports={
+//     // database: 'mongodb://localhost:27017/Library',
+//     database: 'mongodb+srv://asa:ZWiJnhqZECxuK8J9@cluster0.qgsdgt0.mongodb.net/?retryWrites=true&w=majority',
+//     secret: 'yoursecret'
+// }
