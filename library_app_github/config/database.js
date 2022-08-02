93e7e810c7eb6070931e9afe72cf5f28d9c15497
S Nandhakishor <nandakishornair17@gmail.com>
2022-08-02 15:46:21 +0530

    Delete database.js

--- library_app_github/config/database.js
+++
@@ -1,5 +0,0 @@
-// module.exports={
-//     // database: 'mongodb://localhost:27017/Library',
-//     database: 'mongodb+srv://asa:ZWiJnhqZECxuK8J9@cluster0.qgsdgt0.mongodb.net/?retryWrites=true&w=majority',
-//     secret: 'yoursecret'
-// }
